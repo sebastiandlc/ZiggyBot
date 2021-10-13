c24a81e690b5bfe4467e29fed71d38b6fae3596d
Sebastian <sebastian.dlc@outlook.com>
2021-10-13 14:57:36 -0400

    Pulls from imgur album containing our cats

--- cmds/cat.js
+++ cmds/cat.js
@@ -3,17 +3,39 @@
 // found at https://github.com/node-fetch/node-fetch/blob/main/README.md#loading-and-configuring-the-module
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const { IMGUR_CLIENT_ID, IMGUR_ALBUM_HASH } = require('../constants.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('cat')
-        .setDescription('Fetch a random cat image!'),
+        .setDescription('Fetch a random image of one of our cats.'),
     async execute(interaction) {
         try {
+            let albumSize, selectedImage;
             await interaction.deferReply();
-            const { file } = await fetch('https://aws.random.cat/meow').then(response => response.json());
-            interaction.editReply(file);
-            console.log(file);
+
+            // Fetch the imgur album
+            const response = await fetch(`https://api.imgur.com/3/album/${IMGUR_ALBUM_HASH}/images`,
+            {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Client-ID ${IMGUR_CLIENT_ID}`
+                }
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+
+            const data = await response.json();
+
+            // Select a random image from the album
+            albumSize = data.data.length;
+            selectedImage = Math.floor(Math.random() * albumSize);
+
+            // Return the selected image
+            console.log(data.data[selectedImage]);
+            interaction.editReply(`${data.data[selectedImage].link}`);
 
         } catch (error) {
             console.error(error);
--- constants.js
+++ constants.js
@@ -2,5 +2,7 @@ module.exports = Object.freeze({
     CLIENT_ID: process.env.CLIENT_ID,
     GUILD_ID: process.env.GUILD_ID,
     TOKEN: process.env.TOKEN,
-    OW_TOKEN: process.env.OW_TOKEN
+    OW_TOKEN: process.env.OW_TOKEN,
+    IMGUR_CLIENT_ID: process.env.IMGUR_CLIENT_ID,
+    IMGUR_ALBUM_HASH: process.env.IMGUR_ALBUM_HASH
 });
